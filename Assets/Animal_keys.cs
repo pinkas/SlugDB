b1cf01564ee724f7a3c740ecc5b1c739978dd60e
Ben <benoit.pinkasfeld@gmail.com>
2020-08-13 21:02:03 +1000

    Add WriteToUnityProject - small Refactoring Key file generation

---
+++ Assets/Animal_keys.cs
@@ -0,0 +1,9 @@
+using System.Linq;
+using SlugDB;
+
+public class AnimalTable : Table<Animal>
+{
+    public static Animal Lion => Find("Lion", false);
+    public static Animal Monkey => Find("Monkey", false);
+    public static Animal Platypus => Find("Platypus", false);
+}
--- Assets/Person_keys.cs
+++ Assets/Person_keys.cs
@@ -3,14 +3,12 @@ using SlugDB;
 
 public class PersonTable : Table<Person>
 {
-    public static Person fxxjefqv => Table<Person>.Find("fxxjefqv", false);
-    public static Person pibftuqha => Table<Person>.Find("pibftuqha", false);
-    public static Person zblcfawq => Table<Person>.Find("zblcfawq", false);
-    public static Person dzzmbl => Table<Person>.Find("dzzmbl", false);
-    public static Person ixmqpeq => Table<Person>.Find("ixmqpeq", false);
-    public static Person hzijmmxxda => Table<Person>.Find("hzijmmxxda", false);
-    public static Person kjrgcbr => Table<Person>.Find("kjrgcbr", false);
-    public static Person arfkehb => Table<Person>.Find("arfkehb", false);
-    public static Person rypyeyf => Table<Person>.Find("rypyeyf", false);
-    public static Person oscfzrdcd => Table<Person>.Find("oscfzrdcd", false);
+    public static Person Marge => Find("Marge", false);
+    public static Person Homer => Find("Homer", false);
+    public static Person Bart => Find("Bart", false);
+    public static Person Lisa => Find("Lisa", false);
+    public static Person Maggie => Find("Maggie", false);
+    public static Person Moe => Find("Moe", false);
+    public static Person Barney => Find("Barney", false);
+    public static Person Krusty => Find("Krusty", false);
 }
